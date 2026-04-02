c6c506c10f8ec791d39edfe95c88430ddc696fc7
Supheria <supheria@gmail.com>
2026-04-02 12:06:42 +0800

    可以通过 H 键设置 HUD

--- src/main/java/com/supheria/solar_apocalypse_core/client/ClientEventHandler.java
+++ src/main/java/com/supheria/solar_apocalypse_core/client/ClientEventHandler.java
@@ -15,10 +15,10 @@ import com.supheria.solar_apocalypse_core.client.hud.SolarHudEditScreen;
  * 客户端事件处理器
  * 处理快捷键输入等客户端事件
  */
-@Mod.EventBusSubscriber(value = Dist.CLIENT)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientEventHandler {
 
-    private static final KeyMapping HUD_EDIT_KEY = new KeyMapping(
+    public static final KeyMapping HUD_EDIT_KEY = new KeyMapping(
         "key.solar_apocalypse_core.hud_edit",
         InputConstants.KEY_H,
         "key.categories.solar_apocalypse_core"
@@ -28,10 +28,17 @@ public class ClientEventHandler {
     public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(HUD_EDIT_KEY);
     }
+}
 
+/**
+ * 输入事件处理器
+ * 监听键盘输入
+ */
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
+class KeyInputHandler {
     @SubscribeEvent
     public static void onKeyInput(InputEvent.Key event) {
-        if (HUD_EDIT_KEY.consumeClick()) {
+        if (ClientEventHandler.HUD_EDIT_KEY.consumeClick()) {
             Minecraft minecraft = Minecraft.getInstance();
             // 只在有世界且不在其他屏幕打开时才打开编辑屏幕
             if (minecraft.level != null) {
