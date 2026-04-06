04bc0f7e7c6f7b8e70b73778fff2a308de86937e
Supheria <supheria@gmail.com>
2026-04-06 20:06:51 +0800

    添加开局日记本

--- src/main/java/com/supheria/solar_apocalypse_core/network/SolarModVariables.java
+++ src/main/java/com/supheria/solar_apocalypse_core/network/SolarModVariables.java
@@ -11,12 +11,20 @@ import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
 
 import java.util.function.Supplier;
+
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import com.supheria.solar_apocalypse_core.SolarApocalypseCoreMod;
 import com.supheria.solar_apocalypse_core.world.SolarStage;
@@ -50,6 +58,8 @@ public class SolarModVariables {
 	 */
 	@Mod.EventBusSubscriber
 	public static class EventBusVariableHandlers {
+		private static final String STARTER_DIARY_FLAG = "received_starter_diary";
+
 		@SubscribeEvent
 		public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
 			if (!event.getEntity().level().isClientSide()) {
@@ -59,6 +69,15 @@ public class SolarModVariables {
 					mapVariables.syncToPlayer((ServerPlayer) event.getEntity());
 				if (worlddata != null)
 					SolarApocalypseCoreMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) event.getEntity()), new SavedDataSyncMessage(1, worlddata));
+				grantStarterDiaryIfNeeded((ServerPlayer) event.getEntity());
+			}
+		}
+
+		@SubscribeEvent
+		public static void onPlayerClone(PlayerEvent.Clone event) {
+			CompoundTag originalData = event.getOriginal().getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
+			if (!originalData.isEmpty()) {
+				event.getEntity().getPersistentData().put(Player.PERSISTED_NBT_TAG, originalData.copy());
 			}
 		}
 
@@ -69,6 +88,37 @@ public class SolarModVariables {
 				if (worlddata != null)
 					SolarApocalypseCoreMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) event.getEntity()), new SavedDataSyncMessage(1, worlddata));
 			}
+		}
+
+		private static void grantStarterDiaryIfNeeded(ServerPlayer player) {
+			CompoundTag persistedData = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
+			if (persistedData.getBoolean(STARTER_DIARY_FLAG)) {
+				return;
+			}
+			ItemHandlerHelper.giveItemToPlayer(player, createStarterDiary());
+			persistedData.putBoolean(STARTER_DIARY_FLAG, true);
+			player.getPersistentData().put(Player.PERSISTED_NBT_TAG, persistedData);
+		}
+
+		private static ItemStack createStarterDiary() {
+			ItemStack diary = new ItemStack(Items.WRITABLE_BOOK);
+			diary.setHoverName(Component.literal("一本从天而降的日记"));
+			CompoundTag tag = diary.getOrCreateTag();
+			ListTag pages = new ListTag();
+			pages.add(StringTag.valueOf("第%天：\n\n不知道从什么时候开始世界变得不一样了，以前我并没有记日记的习惯，因此我也搞不清问题到底出在哪一天，就更不可能知道问题出在哪里了。"));
+			pages.add(StringTag.valueOf("村庄的房屋有时会莫名其妙的着火，周围的牲畜也越来越少，农夫们抱怨地里的庄稼长得越来越慢，渔夫们则常年打不出几条鱼，那些掠夺者似乎对我们失去了兴趣。"));
+			pages.add(StringTag.valueOf("许多人开始主动去找僵尸被感染，然后被铁傀儡杀掉，这些家伙可真奇怪，变成那群没有感觉的僵尸有什么好的？"));
+			pages.add(StringTag.valueOf("现在大家只需要担心那些蠢笨的僵尸了，那群家伙根本不可能赢过铁傀儡呀，除了天气越来越热之外，日子反而更好过了才对。"));
+			pages.add(StringTag.valueOf("第@天：\n\n附近的矿洞又塌方了，但这片人工矿道相当牢固，从我记事起他就已经存在了，为什么突然塌掉了呢？据说还压死了下矿的玩家，不过他们总会复活的吧。"));
+			pages.add(StringTag.valueOf("玩家比我们村民幸运太多了，他们拥有破坏和创造的能力，简直就像是被神明赐福了一般，我们却只能在村庄里苟延残喘。"));
+			pages.add(StringTag.valueOf("我在图书管理员的家中翻到过一本古籍，据说很久以前我们村民也拥有和玩家一样的创造能力，这些古人在地下建立村庄，只为躲避来自地上的灾祸。"));
+			pages.add(StringTag.valueOf("那里没有玩家会把我们的粮食洗劫一空，也不会有劫掠者和僵尸残害我们的生命……但每当我把这些事情跟其他人分享，他们都嘲笑我是个傻子，连这种传说都信，可他们自己不也相信神明是存在的吗？"));
+			pages.add(StringTag.valueOf("第#天：\n\n井水似乎在枯竭？是因为太久都没有下雨了吗？好在天无绝人之路，旅行的商人告诉我们他来时远方正雷电交加，或许要不了多久村里就又有水源了吧。"));
+			pages.add(StringTag.valueOf("但牧师老村长却大惊小怪，不仅把全村的盔甲、武器商们都叫过来，还说要离开这里。"));
+			pages.add(StringTag.valueOf("我肯定不像他们那么傻，现在外边僵尸那么多，铁傀儡又不会跟他们走，到了晚上都会被吃掉脑子的。"));
+			pages.add(StringTag.valueOf("幸好他们也没拉着我一块走，有铁傀儡保护至少在村子里我是绝对安全的！"));
+			tag.put("pages", pages);
+			return diary;
 		}
 	}
 
